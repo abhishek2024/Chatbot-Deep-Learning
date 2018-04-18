bde0036c4d24be11fb79820433c3da2dda97abd3
yurakuratov <yurakuratov@gmail.com>
2018-04-18 19:15:13 +0300

    feat: add tags2events scorer

---
+++ deeppavlov/models/kg_ranker/kg_tags2events_scorer.py
@@ -0,0 +1,41 @@
+"""
+Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import nltk
+import numpy as np
+
+from deeppavlov.core.models.component import Component
+from deeppavlov.core.common.registry import register
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+@register("kg_tags2events_scorer")
+class KGTags2EventsScorer(Component):
+    def __init__(self, data, *args, **kwargs):
+        self.data = data['places_events']
+
+    def __call__(self, batch, *args, **kwargs):
+        batch_scores = []
+        for tag_scores in batch:
+            scores = {}
+            for event in self.data:
+                scores[event['local_id']] = 0
+                for tag in event['tags']:
+                    scores[event['local_id']] += tag_scores.get(tag, 0)
+            batch_scores.append(scores)
+        return batch_scores
