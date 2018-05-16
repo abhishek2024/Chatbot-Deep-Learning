2ee4f948d15f57702496036517b832f05f8d6353
Olga <ol.gure@gmail.com>
2018-05-16 18:33:59 +0300

    debug

--- deeppavlov/skills/odqa/tfhub_sentence_ranker.py
+++ deeppavlov/skills/odqa/tfhub_sentence_ranker.py
@@ -13,24 +13,26 @@ from deeppavlov.core.models.component import Component
 class TFHUBSentenceRanker(Component):
     def __init__(self, top_k=20, **kwargs):
         self.embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/1")
-        # self.session = tf.Session()
-        # self.session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        self.session = tf.Session()
+        self.session.run([tf.global_variables_initializer(), tf.tables_initializer()])
         self.top_k = top_k
 
     def __call__(self, query_cont: List[Tuple[str, List[str]]]):
         predictions = []
         for el in query_cont:
             # DEBUG
-            print('counting embedding for query')
-            start_time = time.time()
+            # print('counting embedding for query')
             query_emb = self.embed([el[0]])
-            print("Time spent for query emb counting: {} s".format(time.time() - start_time))
-            start_time = time.time()
-            print('counting embedding for contexts')
+            # print("Time spent for query emb counting: {} s".format(time.time() - start_time))
+            # start_time = time.time()
+            # print('counting embedding for contexts')
             cont_embs = self.embed(el[1])
-            print("Time spent for context emb counting: {} s".format(time.time() - start_time))
-            print('counting scores')
-            scores = (query_emb @ np.array(cont_embs).T).squeeze()
+            # print("Time spent for context emb counting: {} s".format(time.time() - start_time))
+            # print('counting scores')
+            start_time = time.time()
+            qe, ce = self.session.run([query_emb, cont_embs])
+            print("Time spent: {}".format(time.time() - start_time))
+            scores = (qe @ ce.T).squeeze()
             top_ids = np.argsort(scores)[::-1][:self.top_k]
             predictions.append([el[1][x] for x in top_ids])
         res = [' '.join(sentences) for sentences in predictions]
