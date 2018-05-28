9d37cbeee8fefe72561c71b75e56b661d4cf9f65
Olga <ol.gure@gmail.com>
2018-05-28 14:51:25 +0300

    feat: add odqa reader which build db

---
+++ deeppavlov/dataset_readers/odqa_reader.py
@@ -0,0 +1,122 @@
+"""
+Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import json
+import logging
+from pathlib import Path
+import unicodedata
+import sqlite3
+from typing import Union, List, Tuple
+
+from overrides import overrides
+from multiprocessing import Pool as ProcessPool
+from tqdm import tqdm
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.data.dataset_reader import DatasetReader
+
+logger = logging.getLogger(__name__)
+
+
+@register('odqa_reader')
+class ODQADataReader(DatasetReader):
+
+    def __init__(self, save_path: str):
+        """
+
+        :param save_path: a path to an output SQLite DB
+        """
+        self.save_path = save_path
+
+    @overrides
+    def read(self, data_path: Union[Path, str], *args, **kwargs) -> None:
+        """
+        :param data_path: a folder with wikiextractor-formatted files
+        :return: None
+        """
+        # DEBUG
+        # if Path(self.save_path).exists():
+        #     Path(self.save_path).unlink()
+
+        logger.info('Reading files...')
+        self._build_db(data_path)
+
+        # DEBUG
+        # if Path(self.save_path).exists():
+        #     Path(self.save_path).unlink()
+
+    def iter_files(self, path: Union[Path, str]):
+        for item in Path(path).iterdir():
+            if item.is_file():
+                yield item
+            elif item.is_dir():
+                yield self.iter_files(item)
+            else:
+                raise RuntimeError("Path doesn't exist: {}".format(item))
+
+    def _build_db(self, data_path: Union[Path, str], num_workers=1):
+        logger.info('Building the database...')
+        conn = sqlite3.connect(self.save_path)
+        c = conn.cursor()
+        sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
+        c.execute(sql_table)
+
+        files = (f for f in self.iter_files(data_path))
+        workers = ProcessPool(num_workers)
+        with tqdm(total=len(data_path)) as pbar:
+            for data in tqdm(workers.imap_unordered(self._get_file_contents, files)):
+                c.executemany("INSERT INTO documents VALUES (?,?)", data)
+                pbar.update()
+        conn.commit()
+        conn.close()
+
+    def _get_json_contents(self) -> List[Tuple[str, str]]:
+        """
+        Read a single json file.
+        :return: file contents
+        """
+
+        docs = []
+        with open(self.save_path) as fin:
+            for line in fin:
+                data = json.loads(line)
+                for doc in data:
+                    if not doc:
+                        continue
+                    title = doc['title']
+                    normalized_title = unicodedata.normalize('NFD', title)
+                    text = doc['text']
+                    normalized_text = unicodedata.normalize('NFD', text)
+                    docs.append((normalized_title, normalized_text))
+        return docs
+
+    def _get_file_contents(self, path) -> List[Tuple[str, str]]:
+        """
+        Read a single wikipedia-extractor formatted file.
+        :param path: path to a wikipedia-formatted file
+        :return: file contents
+        """
+        docs = []
+        with open(path) as fin:
+            for line in fin:
+                doc = json.loads(line)
+                if not doc:
+                    continue
+                title = doc['title']
+                normalized_title = unicodedata.normalize('NFD', title)
+                text = doc['text']
+                normalized_text = unicodedata.normalize('NFD', text)
+                docs.append((normalized_title, normalized_text))
+        return docs
