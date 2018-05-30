9538e6c18a0857407fb373fe8f451415aec962a8
Olga <ol.gure@gmail.com>
2018-05-30 14:35:24 +0300

    fix: iter files

--- deeppavlov/dataset_readers/odqa_reader.py
+++ deeppavlov/dataset_readers/odqa_reader.py
@@ -26,6 +26,7 @@ from tqdm import tqdm
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.data.dataset_reader import DatasetReader
+from deeppavlov.core.commands.utils import expand_path
 
 logger = logging.getLogger(__name__)
 
@@ -33,9 +34,8 @@ logger = logging.getLogger(__name__)
 @register('odqa_reader')
 class ODQADataReader(DatasetReader):
 
-    def __init__(self, save_path: str):
+    def __init__(self, save_path: str, **kwargs):
         """
-
         :param save_path: a path to an output SQLite DB
         """
         self.save_path = save_path
@@ -58,24 +58,26 @@ class ODQADataReader(DatasetReader):
         #     Path(self.save_path).unlink()
 
     def iter_files(self, path: Union[Path, str]):
-        for item in Path(path).iterdir():
-            if item.is_file():
-                yield item
-            elif item.is_dir():
+
+        path = Path(path)
+        if path.is_file():
+            yield path
+        elif path.is_dir():
+            for item in path.iterdir():
                 yield self.iter_files(item)
-            else:
-                raise RuntimeError("Path doesn't exist: {}".format(item))
+        else:
+            raise RuntimeError("Path doesn't exist: {}".format(path))
 
     def _build_db(self, data_path: Union[Path, str], num_workers=1):
         logger.info('Building the database...')
-        conn = sqlite3.connect(self.save_path)
+        conn = sqlite3.connect(str(expand_path(self.save_path)))
         c = conn.cursor()
         sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
         c.execute(sql_table)
 
-        files = (f for f in self.iter_files(data_path))
+        files = [f for f in self.iter_files(data_path)]
         workers = ProcessPool(num_workers)
-        with tqdm(total=len(data_path)) as pbar:
+        with tqdm(total=len(files)) as pbar:
             for data in tqdm(workers.imap_unordered(self._get_file_contents, files)):
                 c.executemany("INSERT INTO documents VALUES (?,?)", data)
                 pbar.update()
@@ -102,7 +104,8 @@ class ODQADataReader(DatasetReader):
                     docs.append((normalized_title, normalized_text))
         return docs
 
-    def _get_file_contents(self, path) -> List[Tuple[str, str]]:
+    @staticmethod
+    def _get_file_contents(path) -> List[Tuple[str, str]]:
         """
         Read a single wikipedia-extractor formatted file.
         :param path: path to a wikipedia-formatted file
