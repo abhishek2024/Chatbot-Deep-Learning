65c5688467ef71262d9799182d3b64556cc848ff
Olga <ol.gure@gmail.com>
2018-06-05 19:29:02 +0300

    fix: multiprocessing, different input formats

--- deeppavlov/dataset_readers/odqa_reader.py
+++ deeppavlov/dataset_readers/odqa_reader.py
@@ -19,113 +19,88 @@ from pathlib import Path
 import unicodedata
 import sqlite3
 from typing import Union, List, Tuple, Generator, Any
+from multiprocessing import Pool
 
-from overrides import overrides
-from multiprocessing import Pool as ProcessPool
 from tqdm import tqdm
 
-from deeppavlov.core.common.registry import register
-from deeppavlov.core.data.dataset_reader import DatasetReader
-from deeppavlov.core.commands.utils import expand_path
 
 logger = logging.getLogger(__name__)
 
 
-@register('odqa_reader')
-class ODQADataReader(DatasetReader):
-
-    def __init__(self, save_path: str, dataset_format='txt', **kwargs):
-        """
-        :param save_path: a path to an output SQLite DB
-        :param dataset_format: format of dataset files, choose from {'txt', 'json', 'wiki', 'sqlite'}
-        """
-        self.save_path = save_path
-        self.dataset_format = dataset_format
-
-    @overrides
-    def read(self, data_path: Union[Path, str], *args, **kwargs) -> None:
-        """
-        :param data_path: a folder with wikiextractor-formatted files
-        :return: None
-        """
-        # DEBUG
-        # if Path(self.save_path).exists():
-        #     Path(self.save_path).unlink()
-
-        logger.info('Reading files...')
-        if self.dataset_format == 'sqlite':
-            return
-        self._build_db(data_path)
-
-        # DEBUG
-        # if Path(self.save_path).exists():
-        #     Path(self.save_path).unlink()
-
-    def iter_files(self, path: Union[Path, str]) -> Generator[Path, Any, Any]:
-        path = Path(path)
-        if path.is_file():
-            yield path
-        elif path.is_dir():
-            for item in path.iterdir():
-                yield self.iter_files(item)
-        else:
-            raise RuntimeError("Path doesn't exist: {}".format(path))
-
-    def _build_db(self, data_path: Union[Path, str], num_workers=1):
-        logger.info('Building the database...')
-        conn = sqlite3.connect(str(expand_path(self.save_path)))
-        c = conn.cursor()
-        sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
-        c.execute(sql_table)
-
-        files = [f for f in self.iter_files(data_path)]
-        workers = ProcessPool(num_workers)
-        with tqdm(total=len(files)) as pbar:
-            if self.dataset_format == 'txt':
-                fn = self._get_file_contents
-            elif self.dataset_format == 'wiki':
-                fn = self._get_wiki_contents
-            elif self.dataset_format == 'json':
-                fn = self._get_json_contents
-            else:
-                raise RuntimeError('Unknown dataset format.')
-            for data in tqdm(workers.imap_unordered(fn, files)):
-                c.executemany("INSERT INTO documents VALUES (?,?)", data)
-                pbar.update()
-        conn.commit()
-        conn.close()
-
-    def _get_json_contents(self) -> List[Tuple[str, str]]:
-        """
-        Read a single json file.
-        :return: tuple of file names and contents
-        """
-
-        docs = []
-        with open(self.save_path) as fin:
-            for line in fin:
-                data = json.loads(line)
-                for doc in data:
-                    if not doc:
-                        continue
-                    title = doc['title']
-                    normalized_title = unicodedata.normalize('NFD', title)
-                    text = doc['text']
-                    normalized_text = unicodedata.normalize('NFD', text)
-                    docs.append((normalized_title, normalized_text))
-        return docs
+class ODQADataReader:
 
     @staticmethod
-    def _get_wiki_contents(path) -> List[Tuple[str, str]]:
-        """
-        Read a single wikipedia-extractor formatted file.
-        :param path: path to a wikipedia-formatted file
-        :return: tuple of file names and contents
-        """
-        docs = []
-        with open(path) as fin:
-            for line in fin:
-                doc = json.loads(line)
+    def read(data_path, *args, **kwargs) -> None:
+        logger.info('Reading files...')
+        if kwargs['dataset_format'] == 'sqlite':
+            return
+        _build_db(kwargs['save_path'], kwargs['dataset_format'], data_path)
+
+
+def iter_files(path: Union[Path, str]) -> Generator[Path, Any, Any]:
+    path = Path(path)
+    if path.is_file():
+        yield path
+    elif path.is_dir():
+        for item in path.iterdir():
+            yield from iter_files(item)
+    else:
+        raise RuntimeError("Path doesn't exist: {}".format(path))
+
+
+def _build_db(save_path, dataset_format, data_path: Union[Path, str], num_workers=4):
+    logger.info('Building the database...')
+    conn = sqlite3.connect(str(save_path))
+    c = conn.cursor()
+    sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
+    c.execute(sql_table)
+
+    files = [f for f in iter_files(data_path)]
+    workers = Pool(num_workers)
+
+    if dataset_format == 'txt':
+        fn = _get_file_contents
+    elif dataset_format == 'json':
+        fn = _get_json_contents
+    elif dataset_format == 'wiki':
+        fn = _get_wiki_contents
+    else:
+        raise RuntimeError('Unknown dataset format.')
+
+    with tqdm(total=len(files)) as pbar:
+        for data in tqdm(workers.imap_unordered(fn, files)):
+            c.executemany("INSERT INTO documents VALUES (?,?)", data)
+            pbar.update()
+
+    conn.commit()
+    conn.close()
+
+    # @staticmethod
+
+
+def _get_file_contents(fpath) -> List[Tuple[str, str]]:
+    """
+    Read a single txt file.
+    :param fpath: path to a txt file
+    :return: tuple of file names and contents
+    """
+    with open(fpath) as fin:
+        text = fin.read()
+        normalized_title = unicodedata.normalize('NFD', fpath.name)
+        normalized_text = unicodedata.normalize('NFD', text)
+        return [(normalized_title, normalized_text)]
+
+
+def _get_json_contents(fpath) -> List[Tuple[str, str]]:
+    """
+    Read a single json file.
+    :return: tuple of file names and contents
+    """
+    docs = []
+    with open(fpath) as fin:
+        for line in fin:
+            data = json.loads(line)
+            for doc in data:
                 if not doc:
                     continue
                 title = doc['title']
@@ -133,18 +108,33 @@ class ODQADataReader(DatasetReader):
                 text = doc['text']
                 normalized_text = unicodedata.normalize('NFD', text)
                 docs.append((normalized_title, normalized_text))
-        return docs
+    return docs
 
-    @staticmethod
-    def _get_file_contents(path) -> List[Tuple[str, str]]:
-        """
-        Read a single txt file.
-        :param path: path to a txt file
-        :return: tuple of file names and contents
-        """
-        with open(path) as fin:
-            text = fin.read()
-            normalized_title = unicodedata.normalize('NFD', path.name)
+
+def _get_wiki_contents(fpath) -> List[Tuple[str, str]]:
+    """
+    Read a single wikipedia-extractor formatted file.
+    :param fpath: path to a wikipedia-formatted file
+    :return: tuple of file names and contents
+    """
+    docs = []
+    with open(fpath) as fin:
+        for line in fin:
+            doc = json.loads(line)
+            if not doc:
+                continue
+            title = doc['title']
+            normalized_title = unicodedata.normalize('NFD', title)
+            text = doc['text']
             normalized_text = unicodedata.normalize('NFD', text)
-            return [(normalized_title, normalized_text)]
+            docs.append((normalized_title, normalized_text))
+    return docs
 
+
+
+
+# reader = ODQADataReader()
+# reader.read(data_path='/media/olga/Data/projects/ODQA/data/PMEF/original/Текст_сбер_корп_ru',
+#             save_path='/media/olga/Data/projects/ODQA/data/pmef_db/ru_sber_corp_11.db',
+#             dataset_format='txt')
+# print('Done!')
