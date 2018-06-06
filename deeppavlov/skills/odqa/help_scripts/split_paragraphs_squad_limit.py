9ed2c668aeac212889bd9e2ea3ece231ba66cc9b
Olga <ol.gure@gmail.com>
2018-06-06 12:54:25 +0300

    feat: output in odqA_reader input format

--- deeppavlov/skills/odqa/help_scripts/split_paragraphs_squad_limit.py
+++ deeppavlov/skills/odqa/help_scripts/split_paragraphs_squad_limit.py
@@ -9,22 +9,45 @@ with open(INPUT_PATH) as fin:
     data = fin.read()
     sentences = sent_tokenize(data)
 
+# all_chunks = {}
+#
+# _len = 0
+# chunks = []
+# for sentence in sentences:
+#     _len += len(sentence.split())
+#     if _len > SQUAD_LIMIT:
+#         all_chunks.append(' '.join(chunks))
+#         _len = 0
+#         chunks = []
+#     chunks.append(sentence)
+#
+# with open(OUTPUT_PATH, 'w') as fout:
+#         # fout.write(' '.join(chunk))
+#         # fout.write('\n')
+#     json.dump(all_chunks, fout)
+#
+#
+# print('Done!')
+
+
 all_chunks = []
 
 _len = 0
 chunks = []
+i = 0
 for sentence in sentences:
     _len += len(sentence.split())
     if _len > SQUAD_LIMIT:
-        all_chunks.append(' '.join(chunks))
+        d = {'text': ' '.join(chunks), 'title': i}
+        all_chunks.append(d)
         _len = 0
         chunks = []
+        i += 1
     chunks.append(sentence)
 
 with open(OUTPUT_PATH, 'w') as fout:
-        # fout.write(' '.join(chunk))
-        # fout.write('\n')
+    # fout.write(' '.join(chunk))
+    # fout.write('\n')
     json.dump(all_chunks, fout)
-
 
 print('Done!')
