7bca759af1d2e901cf990f8a3999c64c1699dd6b
Olga <ol.gure@gmail.com>
2018-07-05 20:56:05 +0300

    fix: sorting is the reason of bad tfidf score, add sort_result attribute

--- deeppavlov/models/supplementary/rank_doc_score_tuplifier.py
+++ deeppavlov/models/supplementary/rank_doc_score_tuplifier.py
@@ -25,15 +25,16 @@ from deeppavlov.core.common.registry import register
 @register("rank_doc_score_tuplifier")
 class RankDocScoreIdTuplifier(Component):
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, sort_result=False, *args, **kwargs):
+        self.sort_result = sort_result
 
     def __call__(self, docs: List[List[str]], scores: List[np.array], ids: List[Any], *args, **kwargs):
         all_results = []
         for triple in zip(docs, scores, ids):
             tuples = list(zip(triple[0], triple[1], triple[2]))
-            tuples.sort(key=itemgetter(1))
-            tuples = tuples[::-1]
+            if self.sort_result:
+                tuples.sort(key=itemgetter(1))
+                tuples = tuples[::-1]
             result = []
             for i, item in enumerate(tuples, 1):
                 item = list(item)
