58e32b59080096e010f660c6056565622cd5b2b2
Olga <ol.gure@gmail.com>
2018-07-27 17:20:16 +0300

    fix: ranker texts slice, comment prints(leads to input/output error when run remotely)

--- deeppavlov/skills/odqa/eval_scripts/evaluate_odqa_via_logits.py
+++ deeppavlov/skills/odqa/eval_scripts/evaluate_odqa_via_logits.py
@@ -11,6 +11,11 @@ import time
 import logging
 from itertools import chain
 from operator import itemgetter
+import sys
+from pathlib import Path
+
+root_path = (Path(__file__) / ".." / ".." / ".." / ".." / "..").resolve()
+sys.path.append(str(root_path))
 
 from nltk.tokenize import sent_tokenize
 
@@ -31,7 +36,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-ranker_config_path", help="path to a JSON ranker config", type=str,
                     default='../../../../deeppavlov/configs/odqa/en_ranker_tfidf_wiki.json')
 parser.add_argument("-dataset_path", help="path to SQuAD dataset", type=str,
-                    default='/media/olga/Data/projects/ODQA/data/squad/preproc/dev-v1.1_prep_4ranker.json')
+                    default='/home/gureenkova/data/squad/dev-v1.1_prep_4odqa.json')
 parser.add_argument("-reader_config_path", help="path to squad reader config", type=str,
                     default='../../../../deeppavlov/configs/squad/squad.json')
 
@@ -42,8 +47,9 @@ def main():
     reader_config = read_json(args.reader_config_path)
     ranker = build_model_from_config(ranker_config)  # chainer
     reader = build_model_from_config(reader_config)
+    # print(reader([("Deep Pavlov killed Kenny.", "Who killed Kenny?")]))
     dataset = read_json(args.dataset_path)
-    # dataset = dataset[:10]
+    # dataset = dataset[:3]
 
     qa_dataset_size = len(dataset)
     logger.info('QA dataset size: {}'.format(qa_dataset_size))
@@ -51,6 +57,7 @@ def main():
     start_time = time.time()
     TEXT_IDX = 1
     SQUAD_LIMIT = 500
+    SQUAD_BATCH_SIZE = 5
 
     try:
         mapping = {}
@@ -60,16 +67,16 @@ def main():
         y_true = list(zip(y_true_text, y_true_start))
 
         ranker_answers = ranker([i['question'] for i in dataset])
-        ranker_texts = [answer[TEXT_IDX] for answer in ranker_answers]
         returned_db_size = len(ranker_answers[0])
         logger.info("Returned DB size: {}".format(returned_db_size))
 
         for n in [5, 10]:
+            # print(f"Starting for {n}")
             y_pred_on_i = []
             i = 1
             for qa, ranker_answer in zip(dataset, ranker_answers):
                 question = qa['question']
-                texts = ranker_texts[:n]
+                texts = [answer[TEXT_IDX] for answer in ranker_answer[:n]]
 
                 # make paragraphs
                 paragraphs = []
@@ -77,15 +84,23 @@ def main():
                     sentences = sent_tokenize(t)
                     chunks = make_chunks(sentences, SQUAD_LIMIT)
                     paragraphs += chunks
-                paragraphs = list(chain.from_iterable(paragraphs))
 
-                questions = question * len(paragraphs)
-                y_pred = reader(paragraphs, questions)
-                best_y_pred = sorted(y_pred, key=itemgetter(3))[0][:2]
-                y_pred_on_i += best_y_pred
+                questions = [question] * len(paragraphs)
+                
+                reader_feed = list(zip(paragraphs, questions))
+                batches = [reader_feed[x:x + SQUAD_BATCH_SIZE] for x in range(0, len(reader_feed), SQUAD_BATCH_SIZE)]
+                y_pred = []
+                for batch in batches:
+                     y_pred += reader(batch)
+                 
+                best_y_pred = sorted(y_pred, key=itemgetter(3), reverse=True)[0][:2]
+                # print(best_y_pred)
+                y_pred_on_i.append(best_y_pred)
                 logger.info(
                     "Processed {} qa pairs for n={}".format(i, n))
                 i += 1
+            # print(y_true)
+            # print(y_pred_on_i)
             total_f1_on_top_i = squad_f1(y_true, y_pred_on_i)
             logger.info(
                 'ODQA f1 for top {}: {}'.format(n, total_f1_on_top_i))
