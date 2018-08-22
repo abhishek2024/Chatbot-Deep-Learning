d28f4b4f49377fe9bd04120193e9d1d1b401f492
dilyararimovna <dilyara.rimovna@gmail.com>
2018-08-22 18:03:06 +0300

    fix: keras_seq2seq_model non working

--- deeppavlov/models/seq2seq/keras_seq2seq_model.py
+++ deeppavlov/models/seq2seq/keras_seq2seq_model.py
@@ -146,6 +146,26 @@ class KerasSeq2SeqModel(KerasModel):
         embeddings_batch = np.asarray(embeddings_batch)
         return embeddings_batch
 
+    def pad_texts(self, sentences: List[List[np.ndarray]], mode="encoder") -> np.ndarray:
+        """
+        Cut and pad tokenized texts to self.opt["text_size"] tokens
+
+        Args:
+            sentences: list of lists of tokens
+            mode: whether to use encoder or decoder padding
+
+        Returns:
+            array of embedded texts
+        """
+        pad = np.zeros(self.opt[mode + '_embedding_size'])
+        if mode == "encoder":
+            text_size = self.opt['src_max_length']
+        elif mode == "decoder":
+            text_size = self.opt['tgt_max_length']
+        cutted_batch = [sen[:text_size] for sen in sentences]
+        cutted_batch = [[pad] * (text_size - len(tokens)) + list(tokens) for tokens in cutted_batch]
+        return np.asarray(cutted_batch)
+
     def lstm_lstm_model(self,
                         hidden_size=300,
                         encoder_coef_reg_lstm=0.,
@@ -167,14 +187,14 @@ class KerasSeq2SeqModel(KerasModel):
                             decoder_rec_dropout_rate)
 
         encoder_decoder_model = Model(inputs=[self._encoder_emb_inp,
-                                              self._decoder_inp],
+                                              self._decoder_emb_inp],
                                       outputs=self._train_decoder_outputs)
 
         self.encoder_model = Model(inputs=self._encoder_emb_inp,
                                    outputs=[self._encoder_state_0,
                                             self._encoder_state_1])
 
-        self.decoder_model = Model(inputs=[self._decoder_inp,
+        self.decoder_model = Model(inputs=[self._decoder_emb_inp,
                                            self._decoder_input_state_0,
                                            self._decoder_input_state_1],
                                    outputs=self._infer_decoder_outputs)
@@ -230,13 +250,14 @@ class KerasSeq2SeqModel(KerasModel):
                        decoder_dropout_rate,
                        decoder_rec_dropout_rate):
 
-        self._decoder_inp = Input(shape=(self.opt["tgt_max_length"],))
+        self._decoder_emb_inp = Input(shape=(self.opt["tgt_max_length"],
+                                             self.opt["decoder_embedding_size"]))
 
-        self._decoder_embedder = Embedding(input_dim=self.opt["decoder_embeddings"].shape[0],
-                                           output_dim=self.opt["decoder_embedding_size"],
-                                           weights=[self.opt["decoder_embeddings"]],
-                                           trainable=False)
-        _decoder_emb_inp = self._decoder_embedder(self._decoder_inp)
+        # self._decoder_embedder = Embedding(input_dim=self.opt["decoder_embeddings"].shape[0],
+        #                                    output_dim=self.opt["decoder_embedding_size"],
+        #                                    weights=[self.opt["decoder_embeddings"]],
+        #                                    trainable=False)
+        # _decoder_emb_inp = self._decoder_embedder(self._decoder_inp)
 
         self._decoder_input_state_0 = Input(shape=(hidden_size,))
         self._decoder_input_state_1 = Input(shape=(hidden_size,))
@@ -252,11 +273,11 @@ class KerasSeq2SeqModel(KerasModel):
             name="decoder_lstm")
 
         _train_decoder_outputs, self._train_decoder_state_0, self._train_decoder_state_1 = decoder_lstm(
-            _decoder_emb_inp,
+            self._decoder_emb_inp,
             initial_state=[self._encoder_state_0, self._encoder_state_1])
 
         _infer_decoder_outputs, self._infer_decoder_state_0, self._infer_decoder_state_1 = decoder_lstm(
-            _decoder_emb_inp,
+            self._decoder_emb_inp,
             initial_state=[self._decoder_input_state_0, self._decoder_input_state_1])
 
         decoder_dense = Dense(self.opt["tgt_vocab_size"], name="dense_lstm")  # (batch_size, text_size, tgt_vocab_size)
@@ -267,37 +288,47 @@ class KerasSeq2SeqModel(KerasModel):
 
     def train_on_batch(self, *args, **kwargs):
         K.set_session(self.sess)
-        dec_outputs = self.one_hotter(dec_outputs, vocab_size=self.opt["tgt_vocab_size"])
+        pad_emb_enc_inputs = self.pad_texts(args[0], mode="encoder")
+        emb_dec_inputs = self.texts2decoder_embeddings([[self.opt["tgt_sos_id"]] + sample + [self.opt["tgt_eos_id"]]
+                                                        for sample in args[1]])
+        pad_emb_dec_inputs = self.pad_texts(emb_dec_inputs, mode="decoder")
+        emb_dec_outputs = self.texts2decoder_embeddings(args[1])
+        pad_emb_dec_outputs = self.pad_texts(emb_dec_outputs, mode="decoder")
 
-        metrics_values = self.model.train_on_batch([enc_inputs,
-                                                    dec_inputs],
-                                                   dec_outputs)
+        metrics_values = self.model.train_on_batch([pad_emb_enc_inputs,
+                                                    pad_emb_dec_inputs],
+                                                   pad_emb_dec_outputs)
         return metrics_values
 
     def infer_on_batch(self, *args, **kwargs):
         K.set_session(self.sess)
-        # self.opt["src_max_length"] = max(src_seq_lengths)
-        self.opt["tgt_max_length"] = None
-        dec_inputs = self._get_decoder_inputs(enc_inputs=enc_inputs)
+        pad_emb_enc_inputs = self.pad_texts(args[0][0], mode="encoder")
+        embedded_eos = self.decoder_embedder([["<EOS>"]])[0][0]
+        embedded_sos = self.decoder_embedder([["<SOS>"]])[0][0]
+        # TODO: no teacher forcing during infer
+        pad_emb_dec_inputs = self.pad_texts([[embedded_eos] + sample + [embedded_sos]
+                                             for sample in args[0]], mode="decoder")
 
-        if dec_outputs:
-            dec_outputs = self.one_hotter(dec_outputs, vocab_size=self.opt["tgt_vocab_size"])
-            _encoder_state_0, _encoder_state_1 = self.encoder_model.predict(enc_inputs)
-            metrics_values = self.decoder_model.test_on_batch([dec_inputs,
+        if len(args[0]) == 2:
+            emb_dec_outputs = self.texts2decoder_embeddings(args[1])
+            pad_emb_dec_outputs = self.pad_texts(emb_dec_outputs, mode="decoder")
+            _encoder_state_0, _encoder_state_1 = self.encoder_model.predict(pad_emb_enc_inputs)
+
+            metrics_values = self.decoder_model.test_on_batch([pad_emb_dec_inputs,
                                                                _encoder_state_0,
                                                                _encoder_state_1],
-                                                              dec_outputs)
+                                                              pad_emb_dec_outputs)
             return metrics_values
         else:
-            _encoder_state_0, _encoder_state_1 = self.encoder_model.predict(enc_inputs)
-            predictions = self._probas2onehot(self.decoder_model.predict([dec_inputs,
+            _encoder_state_0, _encoder_state_1 = self.encoder_model.predict(pad_emb_enc_inputs)
+            predictions = self._probas2onehot(self.decoder_model.predict([pad_emb_dec_inputs,
                                                                           _encoder_state_0,
                                                                           _encoder_state_1]))
             return predictions
 
     def __call__(self, *args, **kwargs):
         K.set_session(self.sess)
-        predictions = np.array(self.infer_on_batch(enc_inputs=enc_inputs))
+        predictions = np.array(self.infer_on_batch(args))
         return predictions
 
     def _probas2onehot(self, data):
@@ -310,17 +341,6 @@ class KerasSeq2SeqModel(KerasModel):
             text_data.append(text_sample)
 
         return np.asarray(text_data)
-
-    def _get_decoder_inputs(self, enc_inputs):
-        dec_inputs = []
-        if type(enc_inputs[0][0]) is int:
-            for sample in enc_inputs:
-                dec_inputs.append(sample[1:] + [self.opt["tgt_eos_id"]])
-        else:
-            for sample in enc_inputs:
-                dec_inputs.append(sample[1:] + self.opt["decoder_embeddings"][self.opt["tgt_eos_id"]])
-
-        return np.asarray(dec_inputs)
 
     def shutdown(self):
         self.sess.close()
