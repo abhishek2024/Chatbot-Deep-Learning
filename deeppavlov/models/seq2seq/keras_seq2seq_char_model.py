37ccdde0d2c3e4e83b80b6c45e69db4feb4cd6cf
dilyararimovna <dilyara.rimovna@gmail.com>
2018-09-24 12:46:47 +0300

    chore: char seq2seq model

--- deeppavlov/models/seq2seq/keras_seq2seq_char_model.py
+++ deeppavlov/models/seq2seq/keras_seq2seq_char_model.py
@@ -45,13 +45,13 @@ class KerasSeq2SeqCharModel(KerasModel):
         hidden_size: size of the hidden layer of encoder and decoder
         source_vocab_size: vocabulary size of source sequences
         target_vocab_size: vocabulary size of target sequences
-        target_start_of_sequence_index: index of start-of-sequence special token in target vocabulary
-        target_end_of_sequence_index: index of end-of-sequence special token in target vocabulary
+        target_start_of_sequence_index: index of start-of-sequence special char in target vocabulary
+        target_end_of_sequence_index: index of end-of-sequence special char in target vocabulary
         encoder_embedding_size: embedding size of encoder's embedder
         decoder_embedder: decoder's embedder component
         decoder_vocab: decoder's vocab component
-        source_max_length: maximal token length of source sequence
-        target_max_length: maximal token length of target sequence
+        source_max_length: maximal char length of source sequence
+        target_max_length: maximal char length of target sequence
         model_name: string name of particular method of this class that builds seq2seq model
         optimizer: string name of optimizer from keras.optimizers
         loss: string name of loss from keras.losses
@@ -75,7 +75,6 @@ class KerasSeq2SeqCharModel(KerasModel):
                  target_start_of_sequence_index: int,
                  target_end_of_sequence_index: int,
                  encoder_embedding_size: int,
-                 decoder_embedder: Component,
                  decoder_vocab: Component,
                  source_max_length: int = None,
                  target_max_length: int = None,
@@ -84,12 +83,12 @@ class KerasSeq2SeqCharModel(KerasModel):
                  loss: str = "binary_crossentropy",
                  lear_rate: float = 0.01,
                  lear_rate_decay: float = 0.,
-                 reinit_lr_with_final_lr: bool = False,
+                 restore_lr: bool = False,
                  **kwargs):
         """
         Initialize model using parameters from config.
         """
-        decoder_embedding_size = kwargs.pop("decoder_embedding_size", decoder_embedder.dim)
+        decoder_embedding_size = kwargs.pop("decoder_embedding_size")
 
         super().__init__(hidden_size=hidden_size,
                          src_vocab_size=source_vocab_size,
@@ -105,7 +104,7 @@ class KerasSeq2SeqCharModel(KerasModel):
                          loss=loss,
                          lear_rate=lear_rate,
                          lear_rate_decay=lear_rate_decay,
-                         reinit_lr_with_final_lr=reinit_lr_with_final_lr,
+                         restore_lr=restore_lr,
                          **kwargs)
 
         # Parameters required to init model
@@ -115,7 +114,6 @@ class KerasSeq2SeqCharModel(KerasModel):
                   "lear_rate": self.opt.get('lear_rate'),
                   "lear_rate_decay": self.opt.get('lear_rate_decay')}
 
-        self.decoder_embedder = decoder_embedder
         self.decoder_vocab = decoder_vocab
 
         self.encoder_model = None
@@ -123,7 +121,7 @@ class KerasSeq2SeqCharModel(KerasModel):
 
         self.model = self.load(model_name=model_name)
 
-        if reinit_lr_with_final_lr:
+        if restore_lr:
             lear_rate = self.opt.get("final_lear_rate", lear_rate)
 
         self.model = self.compile(self.model, optimizer_name=optimizer, loss_name=loss,
@@ -148,7 +146,7 @@ class KerasSeq2SeqCharModel(KerasModel):
                                       loss=loss,
                                       lear_rate=lear_rate,
                                       lear_rate_decay=lear_rate_decay,
-                                      reinit_lr_with_final_lr=reinit_lr_with_final_lr,
+                                      restore_lr=restore_lr,
                                       **kwargs)
         return
 
@@ -182,10 +180,10 @@ class KerasSeq2SeqCharModel(KerasModel):
     def texts2decoder_embeddings(self, sentences: List[List[str]]) -> np.ndarray:
         """
         Convert texts to vector representations using decoder_embedder \
-        and padding up to self.opt["tgt_max_length"] tokens
+        and padding up to self.opt["tgt_max_length"] chars
 
         Args:
-            sentences: list of lists of tokens
+            sentences: list of lists of chars
 
         Returns:
             array of embedded texts
@@ -199,14 +197,6 @@ class KerasSeq2SeqCharModel(KerasModel):
 
         embeddings_batch = np.asarray(embeddings_batch)
         return embeddings_batch
-
-    # pad = np.zeros(self.opt['embedding_size'])
-    #
-    # embeddings_batch = self.fasttext_model([sen[:self.opt['text_size']] for sen in sentences])
-    # embeddings_batch = [[pad] * (self.opt['text_size'] - len(tokens)) + tokens for tokens in embeddings_batch]
-    #
-    # embeddings_batch = np.asarray(embeddings_batch)
-    # return embeddings_batch
 
     def pad_texts(self, sentences: Union[List[List[np.ndarray]], List[List[int]]],
                   text_size: int, embedding_size: int = None) -> np.ndarray:
@@ -267,15 +257,15 @@ class KerasSeq2SeqCharModel(KerasModel):
                             decoder_dropout_rate,
                             decoder_rec_dropout_rate)
 
-        encoder_decoder_model = Model(inputs=[self._encoder_emb_inp,
-                                              self._decoder_emb_inp],
+        encoder_decoder_model = Model(inputs=[self._encoder_inp,
+                                              self._decoder_inp],
                                       outputs=self._train_decoder_outputs)
 
-        self.encoder_model = Model(inputs=self._encoder_emb_inp,
+        self.encoder_model = Model(inputs=self._encoder_inp,
                                    outputs=[self._encoder_state_0,
                                             self._encoder_state_1])
 
-        self.decoder_model = Model(inputs=[self._decoder_emb_inp,
+        self.decoder_model = Model(inputs=[self._decoder_inp,
                                            self._decoder_input_state_0,
                                            self._decoder_input_state_1],
                                    outputs=[self._infer_decoder_outputs,
@@ -302,8 +292,11 @@ class KerasSeq2SeqCharModel(KerasModel):
             None
         """
 
-        self._encoder_emb_inp = Input(shape=(self.opt["src_max_length"],
-                                             self.opt["encoder_embedding_size"]))
+        self._encoder_inp = Input(shape=(self.opt["src_max_length"],))
+
+        _encoder_emb_inp = Embedding(input_dim=self.opt["src_vocab_size"],
+                                     output_dim=self.opt["encoder_embedding_size"],
+                                     input_length=self.opt["src_max_length"])(self._encoder_inp)
 
         self._encoder_outputs, self._encoder_state_0, self._encoder_state_1 = LSTM(
             hidden_size,
@@ -312,7 +305,7 @@ class KerasSeq2SeqCharModel(KerasModel):
             kernel_regularizer=l2(encoder_coef_reg_lstm),
             dropout=encoder_dropout_rate,
             recurrent_dropout=encoder_rec_dropout_rate,
-            name="encoder_lstm")(self._encoder_emb_inp)
+            name="encoder_lstm")(_encoder_emb_inp)
 
         return None
 
@@ -334,10 +327,9 @@ class KerasSeq2SeqCharModel(KerasModel):
             None
         """
 
-        self._decoder_emb_inp = Input(shape=(None,
-                                             self.opt["decoder_embedding_size"]))
-        # self._decoder_emb_inp = Input(shape=(self.opt["tgt_max_length"],
-        #                                      self.opt["decoder_embedding_size"]))
+        self._decoder_inp = Input(shape=(None,))
+        _decoder_emb_inp = Embedding(input_dim=self.opt["tgt_vocab_size"],
+                                     output_dim=self.opt["decoder_embedding_size"])(self._decoder_inp)
 
         self._decoder_input_state_0 = Input(shape=(hidden_size,))
         self._decoder_input_state_1 = Input(shape=(hidden_size,))
@@ -346,18 +338,18 @@ class KerasSeq2SeqCharModel(KerasModel):
             hidden_size,
             activation='tanh',
             return_state=True,  # due to teacher forcing, this state is used only for inference
-            return_sequences=True,  # to get decoder_n_tokens outputs' representations
+            return_sequences=True,  # to get decoder_n_chars outputs' representations
             kernel_regularizer=l2(decoder_coef_reg_lstm),
             dropout=decoder_dropout_rate,
             recurrent_dropout=decoder_rec_dropout_rate,
             name="decoder_lstm")
 
         _train_decoder_outputs, self._train_decoder_state_0, self._train_decoder_state_1 = decoder_lstm(
-            self._decoder_emb_inp,
+            _decoder_emb_inp,
             initial_state=[self._encoder_state_0, self._encoder_state_1])
 
         _infer_decoder_outputs, self._infer_decoder_state_0, self._infer_decoder_state_1 = decoder_lstm(
-            self._decoder_emb_inp,
+            _decoder_emb_inp,
             initial_state=[self._decoder_input_state_0, self._decoder_input_state_1])
 
         decoder_dense = Dense(self.opt["tgt_vocab_size"], name="dense_lstm", activation="softmax")
@@ -371,13 +363,12 @@ class KerasSeq2SeqCharModel(KerasModel):
         Train the self.model on the given batch using teacher forcing
 
         Args:
-            args: list of tokenized text samples
+            args: list of text samples where each sample is a list
             kwargs: additional arguments
 
         Returns:
             metrics values on the given batch
         """
-        K.set_session(self.sess)
         pad_emb_enc_inputs = self.pad_texts(args[0], self.opt["src_max_length"], self.opt["encoder_embedding_size"])
         dec_inputs = [[self.opt["tgt_sos_id"]] + list(sample) + [self.opt["tgt_eos_id"]]
                       for sample in args[1]]  # (bs, ts + 2) of integers (tokens ids)
@@ -391,7 +382,7 @@ class KerasSeq2SeqCharModel(KerasModel):
                                                    pad_emb_dec_outputs)
         return metrics_values
 
-    def infer_on_batch(self, *args: Tuple[Tuple[np.ndarray]], **kwargs) -> List[List[int]]:
+    def infer_on_batch(self, *args: Tuple[Tuple[List[List[int]]]], **kwargs) -> List[List[int]]:
         """
         Infer self.encoder_model and self.decoder_model  on the given data (no teacher forcing)
 
@@ -403,7 +394,6 @@ class KerasSeq2SeqCharModel(KerasModel):
             tokenized indexed decoder predictions
         """
         batch = args[0][0]
-        K.set_session(self.sess)
         pad_emb_enc_inputs = self.pad_texts(batch, self.opt["src_max_length"], self.opt["encoder_embedding_size"])
         encoder_state_0, encoder_state_1 = self.encoder_model.predict(pad_emb_enc_inputs)
 
@@ -430,24 +420,23 @@ class KerasSeq2SeqCharModel(KerasModel):
 
         return predicted_batch
 
-    def __call__(self, *args: Tuple[Tuple[np.ndarray]], **kwargs) -> np.ndarray:
+    def __call__(self, *args: Tuple[List[List[int]]], **kwargs) -> np.ndarray:
         """
         Infer self.encoder_model and self.decoder_model on the given data
 
         Args:
-            *args: encoder inputs (tokenized embedded sentences)
+            *args: encoder inputs
             **kwargs: additional arguments
 
         Returns:
             tokenized indexed decoder predictions
         """
-        K.set_session(self.sess)
         predictions = np.array(self.infer_on_batch(args))
         return predictions
 
     def _probas2ids(self, data: List[List[np.ndarray]]) -> np.ndarray:
         """
-        Convert vectors of probabilities distribution of tokens in the vocabulary \
+        Convert vectors of probabilities distribution of chars in the vocabulary \
         or one-hot token representations in the vocabulary  \
         to corresponding token ids
 
@@ -457,7 +446,7 @@ class KerasSeq2SeqCharModel(KerasModel):
         Returns:
             tokenized samples where each sample is a list of token's id
         """
-        ids_data = np.asarray([[np.argmax(token) for token in sample] for sample in data])
+        ids_data = np.asarray([[np.argmax(char) for char in sample] for sample in data])
 
         return ids_data
 
@@ -473,7 +462,7 @@ class KerasSeq2SeqCharModel(KerasModel):
             tokenized samples where each sample is a list of np.ndarrat of vocabulary size
         """
         onehot = np.eye(vocab_size)
-        onehot_data = np.asarray([[onehot[token] for token in sample] for sample in data])
+        onehot_data = np.asarray([[onehot[char] for char in sample] for sample in data])
 
         return onehot_data
 
