d5487c1121fa1d40fddb9597ec8fb70c67b89d85
Olga Gureenkova <ol.gure@gmail.com>
2018-09-26 15:22:32 +0300

    feat: optimize vectorizing scripts

--- deeppavlov/skills/odqa/help_scripts/vectorize_contexts.py
+++ deeppavlov/skills/odqa/help_scripts/vectorize_contexts.py
@@ -5,26 +5,22 @@ from deeppavlov.skills.odqa.basic_neural_context_encoder import BasicNeuralConte
 
 iterator = SQLiteDataIterator(load_path='/media/olga/Data/projects/DeepPavlov/download/odqa/enwiki_full_chunk.db')
 encoder = BasicNeuralContextEncoder(load_path='/media/olga/Data/projects/DeepPavlov/download/bnr/model')
-SAVE_PATH = '/media/olga/Data/projects/DeepPavlov/download/odqa/chunk_vectors'
+SAVE_PATH = '/media/olga/Data/projects/DeepPavlov/download/odqa/chunk_vectors_{}'
 
-all_vectors = np.empty(shape=(1, 512))
+all_vectors = []
 
 i = 0
 j = 0
-for docs, _ in iterator.gen_batches(batch_size=100):
-    if i == 10000:
-        np.save(SAVE_PATH.format(j), all_vectors)
-        all_vectors = np.empty(shape=(1, 512))
+for docs, _ in iterator.gen_batches(batch_size=1000):
+    if i == 1000:
+        stacked = np.concatenate([all_vectors], axis=1)
+        np.save(SAVE_PATH.format(j), stacked)
+        all_vectors.clear()
         j += 1
         i = 0
     batch_vectors = encoder(docs)
-    all_vectors = np.append(all_vectors, batch_vectors, axis=0)
-    print(all_vectors.shape)
+    all_vectors.append(batch_vectors)
     i += 1
 
-# print(type(all_vectors))
-# print(all_vectors.shape)
 np.save(SAVE_PATH, all_vectors)
-# a = np.load('/media/olga/Data/projects/DeepPavlov/download/odqa/chunk_vectors.npy')
-# print(a)
 
