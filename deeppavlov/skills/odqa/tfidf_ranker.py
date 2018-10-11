e0182e065712eaf3c781d3acfb68bfa1f655fd2b
Olga Gureenkova <ol.gure@gmail.com>
2018-10-11 14:48:39 +0300

    refactor: make tfidf vectorizer responsible for tfidf matrix loading

--- deeppavlov/skills/odqa/tfidf_ranker.py
+++ deeppavlov/skills/odqa/tfidf_ranker.py
@@ -1,73 +1,50 @@
-"""
-Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
-from typing import List
-# import random
+from typing import List, Any, Tuple
 
 import numpy as np
-from scipy.sparse import csr_matrix
-# from sklearn.metrics.pairwise import cosine_similarity
 
-from deeppavlov.core.common.registry import register
 from deeppavlov.core.common.log import get_logger
-from deeppavlov.core.models.estimator import Estimator
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.models.estimator import Component
 from deeppavlov.models.vectorizers.hashing_tfidf_vectorizer import HashingTfIdfVectorizer
 
 logger = get_logger(__name__)
 
 
 @register("tfidf_ranker")
-class TfidfRanker(Estimator):
-    """
-    temporary stub to run REST API
+class TfidfRanker(Component):
+    """Rank documents according to input strings.
+    Args:
+        vectorizer: a vectorizer class
+        top_n: a number of doc ids to return
+        active: whether to return a number specified by :attr:`top_n` (``True``) or all ids
+         (``False``)
+    Attributes:
+        top_n: a number of doc ids to return
+        vectorizer: an instance of vectorizer class
+        active: whether to return a number specified by :attr:`top_n` or all ids
+        index2doc: inverted :attr:`doc_index`
+        iterator: a dataset iterator used for generating batches while fitting the vectorizer
     """
 
-    def get_main_component(self):
-        return self
-
-    def __init__(self, vectorizer: HashingTfIdfVectorizer, top_n=5, active: bool=True, **kwargs):
-        """
-        :param vectorizer: a tfidf vectorizer class
-        :param top_n: top n of document ids to return
-        :param active: when is not active, return all doc ids.
-        """
+    def __init__(self, vectorizer: HashingTfIdfVectorizer, top_n=5, active: bool = True, **kwargs):
 
         self.top_n = top_n
         self.vectorizer = vectorizer
         self.active = active
-
-        if kwargs['mode'] != 'train':
-            if self.vectorizer.load_path.exists():
-                self.tfidf_matrix, opts = self.vectorizer.load()
-                self.ngram_range = opts['ngram_range']
-                self.hash_size = opts['hash_size']
-                self.term_freqs = opts['term_freqs'].squeeze()
-                self.doc_index = opts['doc_index']
-
-                self.vectorizer.doc_index = self.doc_index
-                self.vectorizer.term_freqs = self.term_freqs
-                self.vectorizer.hash_size = self.hash_size
-
-                self.index2doc = self.get_index2doc()
-            else:
-                self.iterator = None
-                logger.warning("TfidfRanker load_path doesn't exist, is waiting for training.")
-
-    def get_index2doc(self):
-        return dict(zip(self.doc_index.values(), self.doc_index.keys()))
 
     def __call__(self, questions: List[str]):
         """
@@ -82,7 +59,7 @@ class TfidfRanker(Estimator):
         q_tfidfs = self.vectorizer(questions)
 
         for q_tfidf in q_tfidfs:
-            scores: csr_matrix = q_tfidf * self.tfidf_matrix
+            scores = q_tfidf * self.vectorizer.tfidf_matrix
             scores = np.squeeze(
                 scores.toarray() + 0.0001)  # add a small value to eliminate zero scores
 
@@ -91,12 +68,12 @@ class TfidfRanker(Estimator):
             # scores = scores + 0.0001
             # scores = scores[0]
 
-            #neg_idx = random.randint(1000, 969792)
+            # neg_idx = random.randint(1000, 969792)
 
             if self.active:
                 thresh = self.top_n
             else:
-                thresh = len(self.doc_index)
+                thresh = len(self.vectorizer.doc_index)
 
             if thresh >= len(scores):
                 o = np.argpartition(-scores, len(scores) - 1)[0:thresh]
@@ -107,7 +84,7 @@ class TfidfRanker(Estimator):
             o_sort = o[np.argsort(-scores[o])]
 
             doc_scores = scores[o_sort]
-            doc_ids = [self.index2doc[i] for i in o_sort]
+            doc_ids = [self.vectorizer.index2doc[i] for i in o_sort]
 
             # neg = self.index2doc[neg]
             # doc_ids.append(neg)
@@ -117,17 +94,3 @@ class TfidfRanker(Estimator):
             batch_docs_scores.append(doc_scores)
 
         return batch_doc_ids, batch_docs_scores
-
-    def fit_batches(self, iterator, batch_size: int):
-        self.vectorizer.doc_index = iterator.doc2index
-        for x, y in iterator.gen_batches(batch_size):
-            self.vectorizer.fit_batch(x, y)
-
-    def fit(self):
-        pass
-
-    def save(self):
-        self.vectorizer.save()
-
-    def load(self):
-        self.vectorizer.load()
