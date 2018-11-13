60a4278448d6fe3846552b6bed94965fb4794a33
mu-arkhipov <arkhipov@yahoo.com>
2018-11-13 21:22:34 +0300

    fix: fix fmeasure for google

--- deeppavlov/metrics/fmeasure.py
+++ deeppavlov/metrics/fmeasure.py
@@ -20,6 +20,7 @@ from itertools import chain
 @register_metric('ner_f1')
 def ner_f1(y_true, y_predicted):
     _, y_predicted = zip(*y_predicted)
+    y_true, _ = zip(*y_true)
     y_true = list(chain(*y_true))
     y_predicted = list(chain(*y_predicted))
     results = precision_recall_f1(y_true,
