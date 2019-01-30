cc98fcf3fc2ebc68327defb202930e1527ac3853
maksimallist <maksimallist@gmail.com>
2019-01-30 17:51:57 +0300

    refactor: rewrite coreference dataset iterator;

--- deeppavlov/dataset_iterators/coreference_iterator.py
+++ deeppavlov/dataset_iterators/coreference_iterator.py
@@ -12,14 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from copy import deepcopy
-from random import Random
-from typing import Tuple, Iterator, Dict, List
+import json
+import linecache
+from copy import copy
+from typing import Dict, Iterator, List, Tuple
+
+from sklearn.model_selection import train_test_split
+
+from deeppavlov.core.common.errors import ConfigError
+from deeppavlov.core.common.log import get_logger
 from deeppavlov.core.common.registry import register
+from deeppavlov.core.data.data_learning_iterator import DataLearningIterator
+
+log = get_logger(__name__)
 
 
 @register("coreference_iterator")
-class CorefIterator:
+class CorefIterator(DataLearningIterator):
     """Dataset iterator for learning models, e. g. neural networks.
 
     Args:
@@ -32,32 +41,76 @@ class CorefIterator:
         random: instance of ``Random`` initialized with a seed
     """
 
-    def split(self, *args, **kwargs):
-        pass
+    def __init__(self, data: Dict, mode: str, seed: int = None, shuffle: bool = True, *args, **kwargs) -> None:
+        super().__init__(data, seed=seed, shuffle=shuffle, *args, **kwargs)
+        self.jsonl_path = None
+        self.mode = mode
+        if self.mode == "jsonl":
+            self.jsonl_path = copy(data['train'])
+            with data['train'].open('r', encoding='utf8') as train_file:
+                data['train'] = [i for i, line in enumerate(train_file) if line.rstrip()]
+        elif self.mode not in ["conll", "jsonl"]:
+            raise ConfigError(f"Coreference iterator not supported '{self.mode}' mode.")
 
-    def __init__(self, data: Dict[str, List[str]], seed: int = None, shuffle: bool = True,
-                 *args, **kwargs) -> None:
-        self.shuffle = shuffle
+        self._split(**kwargs)
 
-        self.random = Random(seed)
+    def _split(self, **kwargs):
+        if kwargs.get("field_to_split") is not None:
+            if kwargs.get("split_fields") is not None:
+                log.info("Splitting field <<{}>> to new fields <<{}>>".format(kwargs.get("field_to_split"),
+                                                                              kwargs.get("split_fields")))
+                self._split_data(field_to_split=kwargs.get("field_to_split"),
+                                 split_fields=kwargs.get("split_fields"),
+                                 split_proportions=[float(s) for s in
+                                                    kwargs.get("split_proportions")],
+                                 split_seed=kwargs.get("split_seed"),
+                                 stratify=kwargs.get("stratify"))
+            else:
+                raise IOError("Given field to split BUT not given names of split fields")
 
-        self.train = data.get('train', [])
-        self.valid = data.get('valid', [])
-        self.test = data.get('test', [])
-        self.split(*args, **kwargs)
-        self.data = {
-            'train': self.train,
-            'valid': self.valid,
-            'test': self.test,
-            'all': self.train + self.test + self.valid
-        }
-
-    def gen_batches(self, batch_size: int = 1, data_type: str = 'train',
-                    shuffle: bool = None) -> Iterator[Tuple[str, str]]:
-        """Generate batches of inputs and expected output to train neural networks
+    def _split_data(self, field_to_split: str = None, split_fields: List[str] = None,
+                    split_proportions: List[float] = None, split_seed: int = None, stratify: bool = None) -> bool:
+        """
+        Split given field of dataset to the given list of fields with corresponding proportions
 
         Args:
-            batch_size: number of samples in batch
+            field_to_split: field name (out of ``"train", "valid", "test"``) which to split
+            split_fields: list of names (out of ``"train", "valid", "test"``) of fields to which split
+            split_proportions: corresponding proportions
+            split_seed: random seed for splitting dataset
+            stratify: whether to use stratified split
+
+        Returns:
+            None
+        """
+        if split_seed is None:
+            split_seed = self.random.randint(0, 10000)
+
+        if field_to_split not in ["train", "valid", "test"]:
+            raise ConfigError(f"Field to split in dataset iterator config must be 'train', 'valid' or 'test', "
+                              f"but '{field_to_split}' was found.")
+
+        data_to_div = self.data[field_to_split].copy()
+        data_size = len(self.data[field_to_split])
+
+        for i in range(len(split_fields) - 1):
+            if stratify:
+                stratify = [sample[1] for sample in data_to_div]
+            self.data[split_fields[i]], data_to_div = train_test_split(
+                data_to_div,
+                test_size=len(data_to_div) - int(data_size * split_proportions[i]),
+                random_state=split_seed,
+                stratify=stratify)
+            self.data[split_fields[-1]] = data_to_div
+        return True
+
+    def gen_batches(self, batch_size: int = 1, data_type: str = 'train',
+                    shuffle: bool = None) -> Iterator[Tuple[tuple, tuple]]:
+        """
+        Generate batches of inputs and expected output to train neural networks
+
+        Args:
+            batch_size:
             data_type: can be either 'train', 'test', or 'valid'
             shuffle: whether to shuffle dataset before batching
 
@@ -66,21 +119,24 @@ class CorefIterator:
         """
         if shuffle is None:
             shuffle = self.shuffle
-
         data = self.data[data_type]
-        data_len = len(data)
+        assert len(data) != 0
 
-        if data_len == 0:
-            return
-
-        order = list(range(data_len))
         if shuffle:
-            self.random.shuffle(order)
+            self.random.shuffle(data)
 
-        for z in order:
-            yield tuple(zip((data[z], deepcopy(data[z]))))
+        if self.mode == 'conll':
+            for conll_file in data:
+                with conll_file.open("r", encoding='utf8') as cnlf:
+                    conll_str = cnlf.read()
+                yield tuple(conll_str, )
+        else:
+            with self.jsonl_path.open('r', encoding='utf8') as json_file:
+                for i in data:
+                    example = json.loads(linecache.getline(json_file, i))
+                    yield example["sentences"], example["speakers"], example["doc_key"], example["clusters"]
 
-    def get_instances(self, data_type: str = 'train') -> Tuple[Tuple[str, str]]:
+    def get_instances(self, data_type: str = 'train') -> Tuple[tuple, tuple]:
         """Get all data for a selected data type
 
         Args:
@@ -89,5 +145,16 @@ class CorefIterator:
         Returns:
              a tuple of all inputs for a data type and all expected outputs for a data type
         """
-        data = list(self.data[data_type])
-        return tuple(zip(data, deepcopy(data)))
+        data = []
+        if self.mode == 'conll':
+            for conll_file in self.data[data_type]:
+                with conll_file.open("r", encoding='utf8') as cnlf:
+                    conll_str = cnlf.read()
+                data.append(conll_str)
+        else:
+            with self.jsonl_path.open('r', encoding='utf8') as json_file:
+                for i in self.data[data_type]:
+                    example = json.loads(linecache.getline(json_file, i))
+                    data.append((example["sentences"], example["speakers"], example["doc_key"], example["clusters"]))
+
+        return tuple(zip(*data))
