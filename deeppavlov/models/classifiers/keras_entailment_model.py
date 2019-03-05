0585180bb319f54f244a679d21106c363bdfd376
dilyararimovna <dilyara.rimovna@gmail.com>
2019-03-05 17:53:58 +0300

    chore: entailment draft

---
+++ deeppavlov/models/classifiers/keras_entailment_model.py
@@ -0,0 +1,346 @@
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from copy import deepcopy
+from logging import getLogger
+from pathlib import Path
+from typing import List, Tuple, Optional, Generator, Union
+
+import keras.metrics
+import keras.optimizers
+import numpy as np
+from keras import backend as K
+from keras.layers import Dense, Input
+from keras.layers import Activation, Concatenate, Add, Multiply, Subtract
+from keras.layers.convolutional import Conv1D
+from keras.layers.core import Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import GlobalMaxPooling1D, MaxPooling1D, GlobalAveragePooling1D
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.wrappers import Bidirectional
+from keras.models import Model
+from keras.regularizers import l2
+from overrides import overrides
+
+from deeppavlov.core.common.errors import ConfigError
+from deeppavlov.core.common.file import save_json, read_json
+from deeppavlov.core.common.registry import register
+from deeppavlov.models.classifiers.keras_classification_model import KerasClassificationModel
+
+log = getLogger(__name__)
+
+
+@register('keras_entailment_model')
+class KerasEntailmentModel(KerasClassificationModel):
+    """
+    Class implements Keras model for entailment task for multi-class multi-labeled pair of texts.
+
+    Args:
+        embedding_size: embedding_size from embedder in pipeline
+        n_classes: number of considered classes
+        model_name: particular method of this class to initialize model configuration
+        optimizer: function name from keras.optimizers
+        loss: function name from keras.losses.
+        learning_rate: learning rate for optimizer.
+        learning_rate_decay: learning rate decay for optimizer
+        last_layer_activation: parameter that determines activation function after classification layer.
+                For multi-label classification use `sigmoid`,
+                otherwise, `softmax`.
+        restore_lr: in case of loading pre-trained model \
+                whether to init learning rate with the final learning rate value from saved opt
+        classes: list or generator of considered classes
+        text_size: maximal length of text in tokens (words),
+                longer texts are cut,
+                shorter ones are padded with zeros (pre-padding)
+        padding: ``pre`` or ``post`` padding to use
+
+    Attributes:
+        opt: dictionary with all model parameters
+        n_classes: number of considered classes
+        model: keras model itself
+        epochs_done: number of epochs that were done
+        batches_seen: number of epochs that were seen
+        train_examples_seen: number of training samples that were seen
+        sess: tf session
+        optimizer: keras.optimizers instance
+        classes: list of considered classes
+        padding: ``pre`` or ``post`` padding to use
+    """
+
+    def __init__(self,
+                 **kwargs):
+        """
+        Initialize model using parameters
+        from opt dictionary (from config), if model is being initialized from saved.
+        """
+
+        super().__init__(**kwargs)
+
+        if isinstance(self.opt["in"], str):
+            self.n_inputs = 1
+        else:
+            self.n_inputs = len(self.opt["in"])
+
+    def pad_texts(self, sentences: List[List[np.ndarray]]) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+        """
+        Cut and pad tokenized texts to self.opt["text_size"] tokens
+
+        Args:
+            sentences: list of lists of tokens
+
+        Returns:
+            array of embedded texts
+        """
+        pad = np.zeros(self.opt['embedding_size'])
+        cutted_batch = [sen[:self.opt['text_size']] for sen in sentences]
+        if self.opt["padding"] == "pre":
+            cutted_batch = [[pad] * (self.opt['text_size'] - len(tokens)) + list(tokens) for tokens in cutted_batch]
+        elif self.opt["padding"] == "post":
+            cutted_batch = [list(tokens) + [pad] * (self.opt['text_size'] - len(tokens)) for tokens in cutted_batch]
+        else:
+            raise ConfigError("Padding type {} is not acceptable".format(self.opt['padding']))
+        return np.asarray(cutted_batch)
+
+    def check_input(self, texts: List[List[np.ndarray]]) -> np.ndarray:
+        """
+        Check and convert input to array of tokenized embedded samples
+
+        Args:
+            texts: list of tokenized embedded text samples
+
+        Returns:
+            array of tokenized embedded texts samples that are cut and padded
+        """
+        if self.opt["text_size"] is not None:
+            features = self.pad_texts(texts)
+        else:
+            if len(texts[0]):
+                features = np.array(texts)
+            else:
+                features = np.zeros((1, 1, self.opt["embedding_size"]))
+
+        return features
+
+    def train_on_batch(self, **kwargs) -> Union[float, List[float]]:
+        """
+        Train the model on the given batch
+
+        Args:
+            texts: list of tokenized embedded text samples
+            labels: list of labels
+
+        Returns:
+            metrics values on the given batch
+        """
+        features = [self.check_input(kwargs[i]) for i in range(self.n_inputs)]
+        labels = kwargs[-1]
+
+        metrics_values = self.model.train_on_batch(features, np.squeeze(np.array(labels)))
+        return metrics_values
+
+    def infer_on_batch(self, **kwargs) -> \
+            Union[float, List[float], np.ndarray]:
+        """
+        Infer the model on the given batch
+
+        Args:
+            texts: list of tokenized embedded text samples
+            labels: list of labels
+
+        Returns:
+            metrics values on the given batch, if labels are given
+            predictions, otherwise
+        """
+        features = [self.check_input(kwargs[i]) for i in range(self.n_inputs)]
+        if len(kwargs) > self.n_inputs:
+            labels = kwargs[-1]
+
+        if labels:
+            metrics_values = self.model.test_on_batch(features, np.squeeze(np.array(labels)))
+            return metrics_values
+        else:
+            predictions = self.model.predict(features)
+            return predictions
+
+    def __call__(self, data: List[List[np.ndarray]], *args) -> List[List[float]]:
+        """
+        Infer on the given data
+
+        Args:
+            data: list of tokenized text samples
+            *args: additional arguments
+
+        Returns:
+            for each sentence:
+                vector of probabilities to belong with each class
+                or list of labels sentence belongs with
+        """
+        preds = np.array(self.infer_on_batch(data), dtype="float64").tolist()
+        return preds
+
+    def cnn_model_max_and_aver_pool(self, kernel_sizes_cnn: List[int], filters_cnn: int, dense_size: int,
+                                    coef_reg_cnn: float = 0., coef_reg_den: float = 0., dropout_rate: float = 0.,
+                                    input_projection_size: Optional[int] = None, **kwargs) -> Model:
+        """
+        Build un-compiled model of shallow-and-wide CNN where average pooling after convolutions is replaced with
+        concatenation of average and max poolings.
+
+        Args:
+            kernel_sizes_cnn: list of kernel sizes of convolutions.
+            filters_cnn: number of filters for convolutions.
+            dense_size: number of units for dense layer.
+            coef_reg_cnn: l2-regularization coefficient for convolutions. Default: ``0.0``.
+            coef_reg_den: l2-regularization coefficient for dense layers. Default: ``0.0``.
+            dropout_rate: dropout rate used after convolutions and between dense layers. Default: ``0.0``.
+            input_projection_size: if not None, adds Dense layer (with ``relu`` activation)
+                                   right after input layer to the size ``input_projection_size``.
+                                   Useful for input dimentionaliry recuction. Default: ``None``.
+            kwargs: other non-used parameters
+
+        Returns:
+            keras.models.Model: uncompiled instance of Keras Model
+        """
+
+        inputs = [Input(shape=(self.opt['text_size'], self.opt['embedding_size']))
+                  for _ in range(self.n_inputs)]
+        outputs = [inputs[i] for i in range(self.n_inputs)]
+
+        if input_projection_size is not None:
+            dense_projection = Dense(input_projection_size, activation='relu')
+            outputs = [dense_projection(outputs[i]) for i in range(self.n_inputs)]
+
+        conv_layers = [Conv1D(filters_cnn, kernel_size=kernel_sizes_cnn[i],
+                              activation=None,
+                              kernel_regularizer=l2(coef_reg_cnn),
+                              padding='same') for i in range(len(kernel_sizes_cnn))]
+        bn = BatchNormalization()
+        relu = Activation("relu")
+        maxpool = GlobalMaxPooling1D()
+        averpool = GlobalAveragePooling1D()
+        concat = Concatenate(axis=-1)
+        concat_1 = Concatenate(axis=1)
+
+        full_outputs = []
+
+        for j in range(self.n_inputs):
+            j_outputs = []
+            for i in range(len(kernel_sizes_cnn)):
+                output_i = conv_layers[i](outputs[j])
+                output_i = bn(output_i)
+                output_i = relu(output_i)
+                output_i_0 = maxpool(output_i)
+                output_i_1 = averpool(output_i)
+                output_i = concat([output_i_0, output_i_1])
+                j_outputs.append(output_i)
+
+            output = concat_1(j_outputs)
+            full_outputs.append(output)
+
+        summ = Add()(full_outputs)
+        mult = Multiply()(full_outputs)
+
+        try:
+            subt = Subtract()(full_outputs)
+            full_outputs.append(subt)
+        except ValueError:
+            pass
+        full_outputs.append(summ)
+        full_outputs.append(mult)
+
+        output = Concatenate()(full_outputs)
+
+        output = Dropout(rate=dropout_rate)(output)
+        output = Dense(dense_size, activation=None,
+                       kernel_regularizer=l2(coef_reg_den))(output)
+        output = BatchNormalization()(output)
+        output = Activation('relu')(output)
+        output = Dropout(rate=dropout_rate)(output)
+        output = Dense(self.n_classes, activation=None,
+                       kernel_regularizer=l2(coef_reg_den))(output)
+        output = BatchNormalization()(output)
+        act_output = Activation(self.opt.get("last_layer_activation", "sigmoid"))(output)
+        model = Model(inputs=inputs, outputs=act_output)
+        return model
+
+    def bigru_with_max_aver_pool_model(self, units_gru: int, dense_size: int,
+                                       coef_reg_gru: float = 0., coef_reg_den: float = 0.,
+                                       dropout_rate: float = 0., rec_dropout_rate: float = 0.,
+                                       **kwargs) -> Model:
+        """
+        Method builds uncompiled model Bidirectional GRU with concatenation of max and average pooling after BiGRU.
+
+        Args:
+            units_gru: number of units for GRU.
+            dense_size: number of units for dense layer.
+            coef_reg_gru: l2-regularization coefficient for GRU. Default: ``0.0``.
+            coef_reg_den: l2-regularization coefficient for dense layers. Default: ``0.0``.
+            dropout_rate: dropout rate to be used after BiGRU and between dense layers. Default: ``0.0``.
+            rec_dropout_rate: dropout rate for GRU. Default: ``0.0``.
+            kwargs: other non-used parameters
+
+        Returns:
+            keras.models.Model: uncompiled instance of Keras Model
+        """
+
+        inputs = [Input(shape=(self.opt['text_size'], self.opt['embedding_size']))
+                  for _ in range(self.n_inputs)]
+        outputs = [inputs[i] for i in range(self.n_inputs)]
+
+        dropout = Dropout(rate=dropout_rate)
+        bigru = Bidirectional(GRU(units_gru, activation='tanh',
+                                  return_sequences=True,
+                                  return_state=True,
+                                  kernel_regularizer=l2(coef_reg_gru),
+                                  dropout=dropout_rate,
+                                  recurrent_dropout=rec_dropout_rate))
+        maxpool = GlobalMaxPooling1D()
+        averpool = GlobalAveragePooling1D()
+        concat = Concatenate(axis=-1)
+
+        full_outputs = []
+
+        for j in range(self.n_inputs):
+            output = dropout(outputs[j])
+
+            output, state1, state2 = bigru(output)
+
+            output1 = maxpool(output)
+            output2 = averpool(output)
+
+            output = concat([output1, output2, state1, state2])
+            full_outputs.append(output)
+
+        summ = Add()(full_outputs)
+        mult = Multiply()(full_outputs)
+
+        try:
+            subt = Subtract()(full_outputs)
+            full_outputs.append(subt)
+        except ValueError:
+            pass
+        full_outputs.append(summ)
+        full_outputs.append(mult)
+
+        output = Concatenate()(full_outputs)
+
+        output = Dropout(rate=dropout_rate)(output)
+        output = Dense(dense_size, activation=None,
+                       kernel_regularizer=l2(coef_reg_den))(output)
+        output = Activation('relu')(output)
+        output = Dropout(rate=dropout_rate)(output)
+        output = Dense(self.n_classes, activation=None,
+                       kernel_regularizer=l2(coef_reg_den))(output)
+        act_output = Activation(self.opt.get("last_layer_activation", "sigmoid"))(output)
+        model = Model(inputs=inputs, outputs=act_output)
+        return model
