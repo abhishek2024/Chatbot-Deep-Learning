cc687981a8b1eaf4971d0f988fc8af5818499522
leonid <puleon@mail.ru>
2019-04-30 15:12:09 +0300

    refactor: change features

--- deeppavlov/models/preprocessors/bert_preprocessor.py
+++ deeppavlov/models/preprocessors/bert_preprocessor.py
@@ -17,7 +17,7 @@ from logging import getLogger
 from typing import Tuple, List, Optional, Union
 
 import numpy as np
-from bert_dp.preprocessing import convert_examples_to_features, InputExample, InputFeatures
+from bert_dp.preprocessing import convert_examples_to_features, _truncate_seq_pair, InputExample, InputFeatures
 from bert_dp.tokenization import FullTokenizer
 
 from deeppavlov.core.commands.utils import expand_path
@@ -238,7 +238,7 @@ class BertRankerPreprocessor(BertPreprocessor):
         for s in cont_resp_pairs:
             ex = [InputExample(unique_id=0, text_a=context, text_b=response) for context, response in s]
             examples.append(ex)
-        features = [convert_examples_to_features(el, self.max_seq_length, self.tokenizer) for el in examples]
+        features = [get_features(el, self.max_seq_length, self.tokenizer) for el in examples]
 
         return features
 
@@ -278,7 +278,7 @@ class BertSepRankerPreprocessor(BertPreprocessor):
             ex = [InputExample(unique_id=0, text_a=text_a, text_b=text_b) for text_a, text_b in
                   zip(s, s_empt)]
             examples.append(ex)
-        features = [convert_examples_to_features(el, self.max_seq_length, self.tokenizer) for el in examples]
+        features = [get_features(el, self.max_seq_length, self.tokenizer) for el in examples]
 
         return features
 
@@ -310,3 +310,84 @@ class BertSepRankerPredictorPreprocessor(BertSepRankerPreprocessor):
             log.info("Building BERT features for the context base...")
             cont_batch = [[el] for el in conts]
             self.cont_features = self(cont_batch)
+
+def get_features(examples, seq_length, tokenizer):
+  """Loads a data file into a list of `InputBatch`s."""
+
+  features = []
+  for (ex_index, example) in enumerate(examples):
+    tokens_a = tokenizer.tokenize(example.text_a)
+
+    tokens_b = None
+    if example.text_b:
+      tokens_b = tokenizer.tokenize(example.text_b)
+
+    if tokens_b:
+      # Modifies `tokens_a` and `tokens_b` in place so that the total
+      # length is less than the specified length.
+      # Account for [CLS], [SEP], [SEP] with "- 3"
+      _truncate_seq_pair(tokens_a, tokens_b, seq_length - 3)
+    else:
+      # Account for [CLS] and [SEP] with "- 2"
+      if len(tokens_a) > seq_length - 2:
+        tokens_a = tokens_a[0:(seq_length - 2)]
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    #  type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+    # (b) For single sequences:
+    #  tokens:   [CLS] the dog is hairy . [SEP]
+    #  type_ids: 0     0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for `type=0` and
+    # `type=1` were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not *strictly* necessary
+    # since the [SEP] token unambiguously separates the sequences, but it makes
+    # it easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
+    tokens = []
+    input_type_ids = []
+    tokens.append("[CLS]")
+    input_type_ids.append(0)
+    for token in tokens_a:
+      tokens.append(token)
+      input_type_ids.append(0)
+    tokens.append("[SEP]")
+    input_type_ids.append(0)
+
+    if tokens_b:
+      for token in tokens_b:
+        tokens.append(token)
+        input_type_ids.append(1)
+      tokens.append("[SEP]")
+      input_type_ids.append(1)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < seq_length:
+      input_ids.append(0)
+      input_mask.append(0)
+      input_type_ids.append(0)
+
+    assert len(input_ids) == seq_length
+    assert len(input_mask) == seq_length
+    assert len(input_type_ids) == seq_length
+
+    features.append(
+        InputFeatures(
+            unique_id=example.unique_id,
+            tokens=tokens,
+            input_ids=input_ids,
+            input_mask=input_mask,
+            input_type_ids=input_type_ids))
+  return features
